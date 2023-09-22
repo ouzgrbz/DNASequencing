8a2efc41d0cb11b38fa021598facf0d7e78e62cf
ouzgrbz <144056876+ouzgrbz@users.noreply.github.com>
2023-09-22 20:29:50 +0300

    Initial commit

---
+++ src/DNA.java
@@ -0,0 +1,25 @@
+//DNAProteinCalculator
+public class DNA {
+    public static void main(String[] args) {
+
+        //  -. .-.   .-. .-.   .
+        //    \   \ /   \   \ /
+        //   / \   \   / \   \
+        //  ~   `-~ `-`   `-~ `-
+        String dna1 = "ATGCGATACGCTTGA";
+        String dna2 = "ATGCGATACGTGA";
+        String dna3 = "ATTAATATGTACTGA";
+        String dna = dna1;
+        System.out.println(dna.length());
+        int start = dna.indexOf("ATG");
+        System.out.println(start);
+        int stop = dna.indexOf("TGA");
+        System.out.println();
+        if(start != -1 && stop != -1 && (stop - start) % 3 == 0){
+            String protein = dna.substring(start, stop+3);
+            System.out.println("Protein Here!: " + protein);
+        } else {
+            System.out.println("No Protein");
+        }
+    }
+}
